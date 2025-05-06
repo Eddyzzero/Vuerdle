5a972b25c0421d124a3e3e324ea3059f4aa330c8
Jhon Florez <jhon.florezr@gmail.com>
2025-05-06 19:42:27 +0200

    api call ramdom category tailwind css init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class=" flex justify-center items-center h-16">
+        <h1>Vuerdle</h1>
+    </header>
+
+    <main>
+        <Word />
+    </main>
+</template>
+
+<script>
+import Word from './components/Word.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Word
+    }
+}
+</script>
--- src/components/Word.vue
+++ src/components/Word.vue
@@ -1,13 +1,27 @@
-<template lang="">
-    <div>
-        
+<template>
+    <div class="flex justify-center items-center p-20">
+        <p class=" tracking-widest" v-if="word">{{ word }}</p>
+        <p v-else>Chargement...</p>
     </div>
 </template>
-<script>
-export default {
 
+<script>
+import { getRandomWord } from '../../wordApi.js';
+
+export default {
+    data() {
+        return {
+            word: null
+        }
+    },
+    async created() {
+        try {
+            this.word = await getRandomWord();
+        } catch (error) {
+            console.error('Erreur lors du chargement du mot:', error);
+        }
+    }
 }
 </script>
-<style lang="">
 
-</style>
+<style></style>
