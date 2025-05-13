71c87d23844d53c33daedf4b9c2042e283987634
RoRo141414 <146880906+RoRo141414@users.noreply.github.com>
2025-05-13 19:00:49 +0200

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,17 @@
 <template>
   <div id="app">
-    <GameView />
   </div>
 </template>
 
 <script>
-import GameView from './components/GameView.vue';
 
 export default {
   name: 'App',
   components: {
-    GameView,
   },
 };
 </script>
 
 <style>
-body {
-  margin: 0;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  background-color: #f4f4f4;
-  color: #333;
-}
+
 </style>
