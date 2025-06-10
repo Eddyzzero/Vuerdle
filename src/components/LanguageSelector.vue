60b01b6c5c889572bce0a73e84aab0ffa56008e3
Jhon Florez <jhon.florezr@gmail.com>
2025-06-10 20:03:41 +0200

    added modal animation

--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -9,7 +9,7 @@
 
         <!-- Menu déroulant -->
         <div v-if="isOpen"
-            class="absolute right-0 mt-2 w-48 rounded-lg shadow-lg bg-amber-200 dark:bg-red-400 ring-1 ring-black ring-opacity-5 z-50">
+            class="absolute right-0 mt-2 w-48 rounded-lg shadow-lg bg-amber-200 dark:bg-red-400 ring-1 ring-black ring-opacity-5 z-50 motion-scale-in-[0.5] motion-translate-x-in-[-120%] motion-translate-y-in-[-60%] motion-opacity-in-[33%] motion-rotate-in-[-1080deg] motion-blur-in-[10px] motion-duration-[0.50s] motion-delay-[0.25s]/scale motion-duration-[0.25s]/opacity motion-duration-[0.80s]/rotate motion-duration-[0.10s]/blur motion-delay-[0.40s]/blur motion-ease-spring-bouncier">
             <div class="py-1 flex flex-col gap-1 m-1">
                 <button @click="selectLanguage('en')"
                     class="w-full px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-amber-100 dark:rounded-lg dark:hover:bg-red-500 flex items-center gap-2"
@@ -36,7 +36,7 @@ export default {
         const supportedLanguages = ['fr', 'en'];
         const savedLanguage = localStorage.getItem('gameLanguage') || 'fr';
         const currentLanguage = supportedLanguages.includes(savedLanguage) ? savedLanguage : 'fr';
-        
+
         return {
             isOpen: false,
             supportedLanguages,
